7221e3a024bf3895dd257ca477aac3f038cd99db
isabellaKulstad <kulstadia>
2019-06-20 16:30:50 -0400

    Iteration One

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Emojional
@@ -9,7 +10,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showMessage(sender: UIButton) {
+    //will be called when a user clicks on the button
+        let alertController = UIAlertController()
+        alertController.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func SadFace(_ sender: Any) {
+     
+       
+    }
+    
+    @IBAction func HappyFace(_ sender:Any) {
+    
+}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +31,8 @@ class ViewController: UIViewController {
 
 
 }
+//("Be happy for this moment. This moment is your life. -Omar Khayyam")
+
+//("Happiness lies in the joy of achievement and the thrill of creative effort. - Franklin Delano Roosevelt")
+// ("There is a sacredness in tears. They are not the mark of weakness, but of power. They speak more eloquently than ten thousand tongues. They are the messengers of overwhelming grief, of deep contrition, and of unspeakable love. - Washington")
 
